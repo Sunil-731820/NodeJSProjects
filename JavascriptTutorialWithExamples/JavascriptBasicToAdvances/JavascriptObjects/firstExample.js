e1253374d99b65f66b22822843a1c49c75f58323
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2025-01-25 00:58:40 +0530

    started the Objects in Js with examples

--- JavascriptTutorialWithExamples/JavascriptBasicToAdvances/JavascriptObjects/firstExample.js
+++ JavascriptTutorialWithExamples/JavascriptBasicToAdvances/JavascriptObjects/firstExample.js
@@ -1,1 +1,22 @@
 console.log("This is the Example of the Js Objects with Examples ")
+var emp ={
+    id : 100,
+    name : "Sunil",
+    Course: "Graduation in CSE"
+}
+
+console.log("the Emp number is : ",emp.id)
+console.log(`the name of the Emp is ${emp.name}`)
+console.log(`The Course of Emp is ${emp.Course}`)
+
+
+//  Creating the Instance oof the Objects okay
+var emp = new Object()
+emp.id = 1000;
+emp.name = "Sunil Kumar Gupta"
+emp.gender = "M"
+
+console.log("The value of the Emp name from the new Objet is :",emp.name)
+console.log("the Gender is from the new Object is :",emp.gender)
+
+
