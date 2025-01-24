8cae583676ce77159c9697c1c93e42d79d5dfbfd
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2025-01-25 00:44:52 +0530

    started the function objects in Js with examples

---
+++ JavascriptTutorialWithExamples/JavascriptBasic/functionExample.js
@@ -0,0 +1,34 @@
+console.log("This is is the Example of the function in JS")
+
+function add(){
+    return 10 + 20
+}
+
+console.log("The Sum of the Two Number is :",add())
+
+function getName(name){
+    console.log(`The name is: ${name}`);
+}
+
+getName("Sunil")
+
+
+function getLastName(lastName){
+    console.log(`the Last name is : ${lastName}`)
+}
+
+getLastName("Gupta")
+
+
+function addTwoString(str1, str2){
+    console.log(`After addition of the two String is :${str1+" "+ str2}`)
+}
+
+addTwoString("Sunil" ,"Kumar")
+
+function multipleParameter(str1 , str2, str3){
+    console.log(`After adding these three String is ${str1 + " "+ str2 + " " + str3}`)
+}
+
+multipleParameter("Sunil","Kumar","Gupta")
+
