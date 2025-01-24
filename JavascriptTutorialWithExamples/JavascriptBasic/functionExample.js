83b7e6ad9f2432aa60dc8756df426512b15a1a21
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2025-01-25 00:49:05 +0530

    started the function objects in Js with examples

--- JavascriptTutorialWithExamples/JavascriptBasic/functionExample.js
+++ JavascriptTutorialWithExamples/JavascriptBasic/functionExample.js
@@ -32,3 +32,14 @@ function multipleParameter(str1 , str2, str3){
 
 multipleParameter("Sunil","Kumar","Gupta")
 
+//  The Use of the Function Object In JS 
+
+var add = new Function("num1","num2", "return num1+num2")
+console.log("The Sum of the Two Numbers using Function Object is :",add(12,23))
+
+
+// Adding the two String Using Function Object 
+
+var addingThetwoStringIs = new Function("str1","str2","return str1 + '  '+str2")
+
+console.log("After adding two String using new Function is :",addingThetwoStringIs("Hi","Bye"))
