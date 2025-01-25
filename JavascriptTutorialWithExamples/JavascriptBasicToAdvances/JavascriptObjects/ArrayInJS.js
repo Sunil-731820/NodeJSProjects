9fd20b5e196756d2b714c3ff0657ef83df1595e5
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2025-01-25 10:36:03 +0530

    started the Array in Js  & its Properties in Js with examples

--- JavascriptTutorialWithExamples/JavascriptBasicToAdvances/JavascriptObjects/ArrayInJS.js
+++ JavascriptTutorialWithExamples/JavascriptBasicToAdvances/JavascriptObjects/ArrayInJS.js
@@ -13,3 +13,63 @@ console.log("using for Loop ")
 for(i=0;i<emp.length;i++){
     console.log(emp[i])
 }
+
+// Array Directly using new keyword 
+
+var arrayName = new Array()
+arrayName[0] = "Java";
+arrayName[1] = "Python";
+arrayName[2] = "C++";
+
+for(i in arrayName){
+    console.log(arrayName[i])
+}
+
+// Using for Loop normally 
+
+for(i=0;i<arrayName.length;i++){
+    console.log(arrayName[i])
+}
+
+
+// using JS Array Constructor
+
+var emp = new Array("Jai","Vijay","Smith")
+console.log("the Size of the Array is :",emp.length)
+
+for(i in emp){
+    console.log(emp[i])
+}
+
+console.log("Using for Loop ")
+for(i =0 ;i<emp.length;i++){
+    console.log(emp[i])
+}
+
+
+//  Use of the concatenate 
+
+var array1 = ["C","C++","Java","Python"]
+var array2 = ["JS","Android","IOS"]
+
+var result = array1.concat(array2)
+console.log("The Size of The Array is :",result.length)
+for(i in result){
+    console.log(result[i])
+}
+
+// Adding the multiple Array in Single Array 
+
+var firstArray = ["C","C++","Java","Python","Ruby"]
+var secondArray = ["Android","Kotlin"]
+var thirdArray = ["IOS","C#"]
+
+console.log("Now Adding the two Array in Single Array okay")
+
+var resultedArray = firstArray.concat(secondArray,thirdArray)
+console.log("the first Array Size is :",resultedArray.length)
+
+for(i in resultedArray){
+    console.log(resultedArray[i])
+}
+
