4bd0dc884aa030ebaea121d55f2a75dd6430736a
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2025-01-25 23:05:05 +0530

    started the String in JS With All functions examples

--- JavascriptTutorialWithExamples/JavascriptBasicToAdvances/JavascriptString/FirstExample.js
+++ JavascriptTutorialWithExamples/JavascriptBasicToAdvances/JavascriptString/FirstExample.js
@@ -16,3 +16,16 @@ console.log(`String name is : ${stringName}`)
 var str11 = "Javascript"
 console.log("The Char at index 4 is :",str11.charAt(4))
 
+
+// use of Search method 
+
+var searchString = "JavaScript is a scripting language. Scripting languages is often interpreted"
+console.log("The Value of the search String by user is ")
+console.log(searchString.search("is"))
+
+// Use of Match functions 
+
+var matchString = "JavaScript"
+console.log("use of the Match functions ")
+console.log(matchString.match("Java"))
+
