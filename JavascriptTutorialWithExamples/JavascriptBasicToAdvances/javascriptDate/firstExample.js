e3e0c4961357beae5c87ca99986c27d77e6db6c6
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2025-01-25 23:10:31 +0530

    started the date and Its built in Function in JS With All functions examples

---
+++ JavascriptTutorialWithExamples/JavascriptBasicToAdvances/javascriptDate/firstExample.js
@@ -0,0 +1,15 @@
+console.log("This is the Example of the Date :")
+var today = new Date()
+console.log("The todays date is :",today)
+
+// Use of the getDate()
+
+var date = new Date();
+var day = date.getDate()
+var month = date.getMonth()+1
+var year = date.getFullYear()
+
+console.log(day + "/" +month+ "/"+ year)
+
+
+
